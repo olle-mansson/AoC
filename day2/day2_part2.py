3d7795c89e353d05133288604e7e9139d4f0d5e3
Olle Månsson <olle.lars.mansson@gmail.com>
2016-12-04 17:05:37 +0100

    Apparently, numpy is not cool to use in such small projects so wrote a Position class and used that instead.

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -1,27 +1,38 @@
-import numpy as np
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Position(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and \
+               self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
 
 
 file = open('input.txt')
-lines = file.readlines()
 code = []
-allowed_positions = {np.array([0, 2]).tostring(): 1, np.array([-1, 1]).tostring(): 2, np.array([0, 1]).tostring(): 3,
-                     np.array([1, 1]).tostring(): 4, np.array([-2, 0]).tostring(): 5, np.array([-1, 0]).tostring(): 6,
-                     np.array([0, 0]).tostring(): 7, np.array([1, 0]).tostring(): 8, np.array([2, 0]).tostring(): 9,
-                     np.array([-1, -1]).tostring(): 'A', np.array([0, -1]).tostring(): 'B',
-                     np.array([1, -1]).tostring(): 'C', np.array([0, -2]).tostring(): 'D'}
+allowed_positions = {Position(0, 2): 1, Position(-1, 1): 2, Position(0, 1): 3,
+                     Position(1, 1): 4, Position(-2, 0): 5, Position(-1, 0): 6,
+                     Position(0, 0): 7, Position(1, 0): 8, Position(2, 0): 9,
+                     Position(-1, -1): 'A', Position(0, -1): 'B',
+                     Position(1, -1): 'C', Position(0, -2): 'D'}
+current_pos = Position(-2, 0)
 
-current_pos = np.array([-2, 0])
-
-for line in lines:
+for line in file:
     for order in line:
-        if order == 'U' and (current_pos + np.array([0, 1])).tostring() in allowed_positions:
-            current_pos += np.array([0, 1])
-        elif order == 'D' and (current_pos + np.array([0, -1])).tostring() in allowed_positions:
-            current_pos += np.array([0, -1])
-        elif order == 'L' and (current_pos + np.array([-1, 0])).tostring() in allowed_positions:
-            current_pos += np.array([-1, 0])
-        elif order == 'R' and (current_pos + np.array([1, 0])).tostring() in allowed_positions:
-            current_pos += np.array([1, 0])
-    code.append(allowed_positions[current_pos.tostring()])
+        if order == 'U' and (current_pos + Position(0, 1)) in allowed_positions:
+            current_pos = current_pos + Position(0, 1)
+        elif order == 'D' and (current_pos + Position(0, -1)) in allowed_positions:
+            current_pos = current_pos + Position(0, -1)
+        elif order == 'L' and (current_pos + Position(-1, 0)) in allowed_positions:
+            current_pos = current_pos + Position(-1, 0)
+        elif order == 'R' and (current_pos + Position(1, 0)) in allowed_positions:
+            current_pos = current_pos + Position(1, 0)
+    code.append(allowed_positions[current_pos])
 
 print(code)
