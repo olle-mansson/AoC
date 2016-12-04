cee9eb8046e0e0cc4ef9736d0920d20ca85b24b6
Olle Månsson <olle.lars.mansson@gmail.com>
2016-12-04 12:00:52 +0100

    Updated the benchmark in part1 to include @hsson updated version. His solution was 4% faster compared to max(), and ~8% faster than sorted()

--- day3/day3_part1.py
+++ day3/day3_part1.py
@@ -9,15 +9,19 @@ def test1():
 
 
 def test2():
-    solution_one(lines=all_lines)
+    solution_two(lines=all_lines)
+
+
+def test3():
+    solution_three(lines=all_lines)
 
 
 def solution_one(lines):
     count = 0
     for line in lines:
-        triangle = [int(x) for x in line.rstrip('\n').split()]
-        sorted_triangle = sorted(triangle)
-        if sorted_triangle[2] < sorted_triangle[0] + sorted_triangle[1]:
+        t = [int(x) for x in line.rstrip('\n').split()]
+        st = sorted(t)
+        if st[2] < st[0] + st[1]:
             count += 1
     return count
 
@@ -25,18 +29,31 @@ def solution_one(lines):
 def solution_two(lines):
     count = 0
     for line in lines:
-        triangle = [int(x) for x in line.rstrip('\n').split()]
-        max_val = max(triangle)
-        triangle.remove(max_val)
-        if max_val < triangle[0] + triangle[1]:
+        t = [int(x) for x in line.rstrip('\n').split()]
+        max_val = max(t)
+        t.remove(max_val)
+        if max_val < t[0] + t[1]:
+            count += 1
+    return count
+
+
+def solution_three(lines):
+    count = 0
+    for line in lines:
+        t = [int(x) for x in line.rstrip('\n').split()]
+        if t[0] + t[1] > t[2] and t[1] + t[2] > t[0] and t[0] + t[2] > t[1]:
             count += 1
     return count
 
 
 print('Solution one took: ', min(
-    timeit.repeat("test1()", setup="from __main__ import test1", number=50, repeat=10)))
+    timeit.repeat("test1()", setup="from __main__ import test1", number=500, repeat=5)))
 print('Solution two took: ', min(
-    timeit.repeat("test2()", setup="from __main__ import test2", number=50, repeat=10)))
-
+    timeit.repeat("test2()", setup="from __main__ import test2", number=500, repeat=5)))
+print('Solution three took: ', min(
+    timeit.repeat("test3()", setup="from __main__ import test3", number=500, repeat=5)))
 # seems like there are no performance penalty to choosing sorted()
-print('\nAnswer:\n', solution_one(all_lines))
+
+print('\nAnswer one:\n', solution_one(all_lines))
+print('\nAnswer two:\n', solution_two(all_lines))
+print('\nAnswer three:\n', solution_three(all_lines))
